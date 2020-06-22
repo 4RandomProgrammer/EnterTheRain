4e83765ead2a6f2cb6c5357e99090e2f7d23a998
ARandomProgrammer <50036343+4RandomProgrammer@users.noreply.github.com>
2020-06-22 19:19:32 -0300

    Revert "Merge remote-tracking branch 'origin/Boss' into PowerUp2-and-Ui"
    This reverts commit c15f8f02db904cb468a0b37377fa25f864607329, reversing
    changes made to 2b2752cc496c1860e34c2039ac0acffa4efd2ef0.

--- Enter the Rain/Assets/Torreta/TurretBullet.gd
+++ Enter the Rain/Assets/Torreta/TurretBullet.gd
@@ -1,25 +1,21 @@
 extends Area2D
 
-export var speed = 700
+var speed = 700
 var velocity = Vector2()
-export (Resource) var bullet_sprite
-export var damage = 1
+
+const DAMAGE = 1
 
 func start(pos, dir):
 	position = pos
 	rotation = dir
 	velocity = Vector2(speed, 0).rotated(dir)
-	if bullet_sprite:
-		$Sprite.texture = bullet_sprite
 
 func _physics_process(delta):
 	position += velocity * delta
 
-
-
-func _on_VisibilityNotifier2D_screen_exited():
+func _on_TurretBullet_body_entered(_body):
 	queue_free()
 
 
-func _on_Area2D_body_entered(_body):
+func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
