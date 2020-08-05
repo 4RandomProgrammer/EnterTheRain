53d8888154a93c31cb3c3df46c30d6ec851dd222
ARandomProgrammer <50036343+4RandomProgrammer@users.noreply.github.com>
2020-08-05 11:05:40 -0300

    Archer

--- Enter the Rain/Assets/PowerUps/ShotUp.gd
+++ Enter the Rain/Assets/PowerUps/ShotUp.gd
@@ -2,8 +2,7 @@ extends Area2D
 
 export var damage = 1
 
-func _ready():
-	$ActivateTimer.start(-1)
+
 
 func _on_ActivateTimer_timeout():
 	$Hitbox/CollisionShape2D.call_deferred("set","disabled",false)
