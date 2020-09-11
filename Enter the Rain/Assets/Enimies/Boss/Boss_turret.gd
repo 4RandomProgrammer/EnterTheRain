e8820713879b3ed32c8e69eb68fc937a8bd3174d
GregorioFornetti <fornetti.greg@gmail.com>
2020-09-10 22:21:18 -0300

    novos poderes boss 5 | mudanças boss 1

--- Enter the Rain/Assets/Enimies/Boss/Boss_turret.gd
+++ Enter the Rain/Assets/Enimies/Boss/Boss_turret.gd
@@ -12,10 +12,13 @@ onready var player = get_parent().get_node('Player')
 onready var Missile_shot = load('res://Assets/Enimies/Enemy_missile.tscn')
 onready var Shield_machine = load('res://Assets/Enimies/Boss/Shield_machine.tscn')
 onready var Big_bullet = load("res://Assets/Enimies/Enemy_bullet/SlowBigBullet.tscn")
+onready var Explosion_target = load("res://Assets/Enimies/Mini_explosion_target.tscn")
 var player_positions_list = []
 var displaying_laser = false
 var ang_player
 var shield_machine_count = 0
+var pulling_count = 0
+var extra_explosions = 0
 # Lista das posições das maquinas protetoras (relativas à posição do Boss).
 var shield_machines_positions = [
 	Vector2(500, 250),  # Canto inferior direito
@@ -26,7 +29,9 @@ var shield_machines_positions = [
 enum {
 	SPAWNING,
 	NORMAL,
-	LASER
+	LASER,
+	PULLING,
+	MANY_BULLETS
 }
 var state = SPAWNING
 
@@ -41,14 +46,25 @@ func _physics_process(delta):
 			SPAWNING:
 				pass
 			NORMAL:
-				ang_player = (player.position - position).angle()
-				rotation = ang_player
+				rotate_turret()
 				shoot()
 			LASER:
 				player_positions_list.append(player.global_position)
 				if displaying_laser:
 					rotation = (player_positions_list[0] - global_position).angle()
 					player_positions_list.pop_front()
+			PULLING:
+				player.position -= (player.position - position).normalized()  # puxar player
+				rotate_turret()
+				shoot()
+			MANY_BULLETS:
+				var big_bullet = Big_bullet.instance()
+				big_bullet.start(global_position, rng.randf_range(0, 360))
+				get_parent().call_deferred('add_child', big_bullet)
+
+func rotate_turret():
+	ang_player = (player.position - position).angle()
+	rotation = ang_player
 
 func shoot():
 	# Atirar na direção do player. As bullets recebem um incremento de -30° até 30° de angulo (pi / 6).
@@ -71,7 +87,9 @@ func _ready():
 
 func _draw():
 	if shield_machine_count != 0:
-		draw_arc(Vector2.ZERO, 50, 0, 360, 1000, ColorN('Green'))
+		draw_arc(Vector2.ZERO, 50, 0, 360, 1000, ColorN('Green'))  # Desenhar escudo de proteção (circulo verde)
+	if state == PULLING and is_instance_valid(player):
+		draw_line(Vector2.ZERO, (player.position - position).rotated(-rotation), ColorN('Yellow')) # Desenhar laser que puxa.
 
 func _on_HurtBox_area_entered(area):
 	if state != SPAWNING and shield_machine_count == 0:
@@ -93,7 +111,7 @@ func _on_Timer_change_pow_timeout():
 	choose_power()
 
 func choose_power():
-	var power_choosen = rng.randi_range(1, 2)
+	var power_choosen = rng.randi_range(1, 4)
 	if power_choosen == 1:
 		# Poder 1: Raio laser. Ação: Iniciar os timers referentes a esse poder e mudar o estado
 		state = LASER
@@ -111,10 +129,15 @@ func choose_power():
 			choose_power()
 	elif power_choosen == 3:
 		# Poder 3: Puxao e explosoes. Ação: começar a puxar o player para a direção da torre e fazer algumas explosões.
-		player.position += (player.position - position).normalized()
+		state = PULLING
+		$Timer_pulling.start()
+	else:
+		# Poder 4: Tiro para todos os lados !
+		$Timer_many_bullets.start()
+		state = MANY_BULLETS
 
 
-func _on_Timer_laser_timeout():
+func _on_Timer_laser_timeout():  # Power 1
 	if not displaying_laser:
 		# Ativar o laser por 5 secs
 		laser_sprite.visible = true
@@ -131,10 +154,38 @@ func _on_Timer_laser_timeout():
 		laser_missile_timer.stop()
 
 
-func _on_Timer_missile_laser_timeout():
+func _on_Timer_missile_laser_timeout():  # Atirando misseis durante poder 1
 	# Soltar um missil e começar a contagem novamente...
 	if is_instance_valid(player):
 		var missile = Missile_shot.instance()
 		missile.start(global_position, (player.position - position).angle())
 		get_parent().call_deferred('add_child', missile)
 		laser_missile_timer.start(rng.randf_range(0.5, 1.75))
+
+
+func _on_Timer_pulling_timeout():  # Power 3
+	if pulling_count != 4 and is_instance_valid(player):
+		var current_angle = 0
+		var target_position = (player.position - position).normalized() * (50 + 100 * extra_explosions)
+		while current_angle <= 2 * PI:
+			spawn_target_explosion(position + target_position.rotated(current_angle))
+			current_angle += PI / 10 / (extra_explosions + 1)
+		extra_explosions += 1
+		if extra_explosions == 6:
+			extra_explosions = 0
+			pulling_count += 1
+		$Timer_pulling.start()
+	else:
+		state = NORMAL
+		pulling_count = 0
+
+
+func spawn_target_explosion(target_pos):
+	var Target = Explosion_target.instance()
+	Target.position = target_pos
+	Target.explosion_timer = 1
+	get_parent().call_deferred('add_child', Target)
+
+
+func _on_Timer_many_bullets_timeout():
+	state = NORMAL
--- Enter the Rain/Assets/Enimies/Explosion_target.gd
+++ Enter the Rain/Assets/Enimies/Explosion_target.gd
@@ -1,6 +1,9 @@
 extends Node2D
 export (Resource) var Spawn
+export var explosion_timer = 3
 
+func _ready():
+	$Timer.start(explosion_timer)
 
 func _on_Timer_timeout():
 	var explosion = Spawn.instance()
