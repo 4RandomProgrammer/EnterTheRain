80e0b7a53de56159af6b729a698b32930ec93cdc
GregorioFornetti <fornetti.greg@gmail.com>
2020-11-19 12:53:11 -0300

    spawn shooter chaser

--- Enter the Rain/Assets/Spawners/Fase1/EnemySpawner1.gd
+++ Enter the Rain/Assets/Spawners/Fase1/EnemySpawner1.gd
@@ -5,7 +5,7 @@ onready var Inimigo = preload("res://Assets/Enimies/Perseguidores/Bee_enemy.tscn
 onready var Besouro = preload('res://Assets/Enimies/InimigosNProntos/melee_attacker.tscn')
 onready var Super_bee = preload('res://Assets/Enimies/Perseguidores/Super_bee.tscn')
 onready var Rusher_shooter = preload('res://Assets/Enimies/Perseguidores/Rusher_Shooter.tscn')
-onready var Enemy_shooter = preload("res://Assets/Enimies/InimigosNProntos/Enemy_shooter.tscn")
+onready var Enemy_shooter = preload("res://Assets/Enimies/Perseguidores/Shooter_chaser.tscn")
 onready var ShootNAtack = preload("res://Assets/Enimies/InimigosNProntos/ShootNAttack.tscn")
 onready var enimies_list = [Torreta, Inimigo, Besouro, Super_bee, Rusher_shooter, Enemy_shooter, ShootNAtack]
 
